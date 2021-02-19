3b681b90e44f06b94411157eded8b1396a6ec00f
nExoR <nexorek@gmail.com>
2021-02-19 10:53:59 +0100

    v1

--- QoD Migration/rename-SPSites.ps1
+++ QoD Migration/rename-SPSites.ps1
@@ -3,22 +3,35 @@
 param (
     #CSV input file
     [Parameter(mandatory=$true,position=0)]
-        [string]$inputCSV
+        [string]$inputCSV,
+    #delimiter
+    [Parameter(mandatory=$false,position=1)]
+        [string]$delimiter=';'
     
 )
 
 $header = @('Site name','URL')
-$data = load-CSV -inputCSV $inputCSV -headerIsCritical -header $header -delimiter ','
+$data = load-CSV -inputCSV $inputCSV -headerIsCritical -header $header -delimiter $delimiter
 
 try { 
-    $sposites=get-sposite 
+    $sposites = get-sposite 
 } catch { 
-    'no connection' 
+    write-log 'no connection' -type error
+    exit -1
 }
 
 foreach($site in $data) {
-    $siteURL = ($site.url).split('/')
-    $siteRealtive = "SDS"+$siteURL[-1]
-    $newSiteURL = 
-    #Start-SPOSiteRename -Identity $site.url -NewSiteUrl 
+    $siteURL = ($site.url).replace('https://','').split('/')
+    $siteRealtive = "/SDS"+$siteURL[-1]
+    $newSiteURL = "{0}{1}{2}" -f 'https://', ($siteURL[0..($siteURL.count - 2)] -join '/'), $siteRealtive
+
+    $newSiteTitle = "SDS $($site.'site name')"
+
+    write-log "site $($site.URL) will be changed to $newSiteURL and title $newSiteTitle" -type info
+    try {
+        Start-SPOSiteRename -Identity $site.url -NewSiteUrl $newSiteURL -NewSiteTitle $newSiteTitle -Confirm:$false
+    } catch {
+        write-log "error renaming site $($_.exception)" -type error
+    }
 }
+write-log "done." -type ok
